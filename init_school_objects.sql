4502615a3b52dd91f4a2eba30f28eaceb6a5e895
dporter14 <dporter14@csub.edu>
2019-03-28 15:28:50 -0700

    init db and shell of init objects

---
+++ init_school_objects.sql
@@ -0,0 +1,95 @@
+/*
+Create all the tables for the school specifying their contstraints
+*/
+\c school --connect to database school
+
+create if not exists employee (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists faculty (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists club (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists department (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists course (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists section (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists student (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists building (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists room (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists equipment (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists locker (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists joins (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
+
+create if not exists attends (
+    -- TO DO:
+    -- Attributes
+    -- constraints
+)
+tablespace schoolData;
---
+++ init_tbspace_db.sql
@@ -0,0 +1,15 @@
+/*
+Create the default, index, and temp tablespaces for school
+*/
+
+create user school superuser password 'bchseagles'; --create role
+
+create tablespace schoolData
+    owner school
+    location 'var/lib/postgresql/dbs/school/data'; --first create empty directory as postgres user (sudo su postgres) or this will fail
+
+create tablespace schoolIdx
+    owner school
+    location 'var/lib/postgresql/dbs/school/idx'; --first create empty directory as postgres user (sudo su postgres) or this will fail
+
+create database school with owner = school tablespace = schoolData;
