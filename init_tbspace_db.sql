8ac7890c6394f021bd733ba464a209f4d060dd0f
dporter14 <dporter14@csub.edu>
2019-05-13 11:29:26 -0700

    fixed db init

--- init_tbspace_db.sql
+++ init_tbspace_db.sql
@@ -2,7 +2,7 @@
 */
 CREATE DATABASE schooldb;
 
-CREATE USER school SUPERUSER PASSWORD 'bchseagles';
+CREATE USER school PASSWORD 'bchseagles';
 
 ALTER ROLE school SET client_encoding TO 'utf8';
 ALTER ROLE school SET default_transaction_isolation TO 'read committed';
